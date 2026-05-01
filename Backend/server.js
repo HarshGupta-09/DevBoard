20f53076b475fb9c800df0a4cb02268b746cba50
Harsh Gupta <harshgupta82003@gmail.com>
2026-05-01 18:50:31 +0530

    fix : cors

--- Backend/server.js
+++ Backend/server.js
@@ -14,7 +14,11 @@ const app = express();
 
 
 app.use(cors({
-  origin: "*",
+  origin: [
+    "http://localhost:5173",
+    "https://dev-board-gules.vercel.app"
+  ],
+  credentials: true
 }));
 
 //Middlwares
